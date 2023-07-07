f09c4301d1d6a2366d2604355813040d3e8711c8
abhishek101077 <Abhishekgargshu786@gmail.com>
2023-07-08 00:09:36 +0530

    [Abhishek]:The all programme file and use case of lambda expression

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace LamdaExpression
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<person> listpersonincity = new List<person>();
+            AddRecords(listpersonincity);
+            // RetriveTwoRecordsislessthensixty(listpersonincity);
+            // RetriveTwoRecordsislessthenthirteen(listpersonincity);
+            // AvaraageAge(listpersonincity);
+            // specific_Name(listpersonincity);
+            RetriveTwoRecordsisgreaterthensixty(listpersonincity);
+
+        }
+        private static void AddRecords(List<person> listpersonincity)
+        {
+            listpersonincity.Add(new person("874562184", "john", "12thmainstreet", 65));
+            listpersonincity.Add(new person("874562174", "smith", "12thmainstreet", 25));
+            listpersonincity.Add(new person("874563194", "Rafael", "12thmainstreet", 35));
+            listpersonincity.Add(new person("874567194", "gavi", "12thmainstreet", 45));
+            listpersonincity.Add(new person("874569194", "pedri", "12thmainstreet", 55));
+            listpersonincity.Add(new person("874562194", "varane", "12thmainstreet", 17));
+            listpersonincity.Add(new person("874563194", "Dybala", "12thmainstreet", 85));
+            listpersonincity.Add(new person("874563194", "Roger", "12thmainstreet", 75));
+        }
+        private static void RetriveTwoRecordsislessthensixty(List<person> listperson)
+        {
+            foreach (person person in listperson.FindAll(a => (a.Age < 60)).Take(4))
+
+            {
+                Console.WriteLine(person.Name + " " + person.Age);
+
+            }
+
+
+        }
+        private static void RetriveTwoRecordsislessthenthirteen(List<person> listperson)
+        {
+            foreach (person person in listperson.FindAll(a => (a.Age < 18 && a.Age > 13)).Take(4))
+
+            {
+                Console.WriteLine(person.Name + " " + person.Age);
+
+            }
+        }
+        private static void AvaraageAge(List<person> listperson)
+        {
+            double avgage = listperson.Average(a => a.Age);
+            Console.WriteLine("The Avarage Age" + avgage);
+        }
+        private static void specific_Name(List<person> listperson)
+        {
+            bool val = listperson.Exists(e => e.Name == "Abhishek");
+            if (val)
+            {
+                Console.WriteLine("Name exists");
+            }
+            else
+            {
+                Console.WriteLine("Name does not exist");
+            }
+        }
+
+        private static void RetriveTwoRecordsisgreaterthensixty(List<person> listperson)
+        {
+            foreach (person person in listperson.FindAll(a => (a.Age > 60)))
+
+            {
+                Console.WriteLine(person.Name + " " + person.Age);
+
+            }
+        }
+    }
+}
+
+              
+             
+                
+             
+
+
+
+        
+    
+
+
+
+          
---
+++ person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpression
+{
+    public class person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        
+        public person(string SSN, string Name, string Address, int age)
+        {
+            this.SSN = SSN;
+            this.Name = Name;
+            Address = Address;
+            Age =  age;
+
+
+          
+        }
+         
+    }
+}
